c181c1fa4d522e8c7db5cef5842f45c1974717b9
red <red54321@outlook.com>
2021-11-15 10:08:20 +0000

    remove old comment

--- service/service.go
+++ service/service.go
@@ -22,7 +22,7 @@ type Service struct {
 	Producer          kafka.IProducer
 	DatasetAPIClient  DatasetAPIClient
 	Processor         Processor
-	S3PrivateUploader S3Uploader // !!! how can this be the same type as the 'public' ?
+	S3PrivateUploader S3Uploader
 	S3PublicUploader  S3Uploader //!!! does this service need an S3 downloader, or can it use this for download as well and maybe rename this to 'S3access' ?
 	VaultClient       VaultClient
 	generator         Generator
