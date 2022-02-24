7e09f4f83fed090106bc2730ce1e50599bed92fd
red <red54321@outlook.com>
2022-02-24 14:23:05 +0000

    Increase shutdown time for CI tests

--- config/config.go
+++ config/config.go
@@ -62,7 +62,7 @@ func Get() (*Config, error) {
 
 	cfg = &Config{
 		BindAddr:                   "localhost:26800",
-		GracefulShutdownTimeout:    5 * time.Second,
+		GracefulShutdownTimeout:    25 * time.Second,
 		HealthCheckInterval:        30 * time.Second,
 		HealthCheckCriticalTimeout: 90 * time.Second,
 		ServiceAuthToken:           "",
